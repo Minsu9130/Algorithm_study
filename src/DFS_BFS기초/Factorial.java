690ed2db680c8795ec6cf74d4ddb4525c63413b3
Minsu <alstntn2631@naver.com>
2022-05-10 00:09:10 +0900

    [Recursive] 팩토리얼

---
+++ src/DFS_BFS기초/Factorial.java
@@ -0,0 +1,19 @@
+package DFS_BFS±‚√ ;
+
+public class Factorial {
+
+	public static void main(String[] args) {
+		
+		System.out.println(DFS(5));
+	} 
+	
+	public static int DFS(int n) {
+		if(n == 1) {
+			return 1;
+		}
+		else {
+			return n*DFS(n-1);
+		}
+	}
+
+}
